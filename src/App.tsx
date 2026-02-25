03210d91032f9cfaf64cbec39dd76d49a1ee12ed
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2026-02-25 03:03:12 +0200

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' , fontSize: '24px', fontWeight: 'bold' }}>
+      let's go to build!
+    </div>
+  )
+}
+
+export default App
