0b7139f5647ca3960f8c036067d55128f7b2133d
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2026-02-25 03:53:22 +0200

    Add static UI components and styles for Kanban board layout
    - Implement SearchAppBar component with search functionality
    - Update App component to include grid layout for task categories
    - Add MUI icons and styles for improved UI
    - Create global CSS for consistent styling across components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import Grid from "@mui/material/Grid"
+import SearchAppBar from "./components/SearchAppBar"
 
 
 function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' , fontSize: '24px', fontWeight: 'bold' }}>
-      let's go to build!
-    </div>
+    <>
+      <SearchAppBar />
+      <Grid container spacing={2} sx={{ backgroundColor: '#f5f5f5', padding: '16px' }}>
+        <Grid size={{ xs: 12, md: 3 }}>
+          <h2>Backlog</h2>
+        </Grid>
+        <Grid size={{ xs: 12, md: 3 }}>
+          <h2>In Progress</h2>
+        </Grid>
+        <Grid size={{ xs: 12, md: 3 }}>
+          <h2>Archived</h2>
+        </Grid>
+        <Grid size={{ xs: 12, md: 3 }}>
+          <h2>Done</h2>
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
