aee15db494ec6bce8591c8593df14d3ec4aee4e4
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2026-02-26 06:29:46 +0200

    Add API CRUD operations for tasks using http client

---
+++ src/lib/api.ts
@@ -0,0 +1,21 @@
+import { http } from "./http";
+import type { Task, TaskCreate, TaskSearchParams, TaskUpdate } from "../types/index";
+
+const TASKS = "/tasks";
+
+export const taskApi = {
+  getAll: (params?: Record<string, TaskSearchParams>) =>
+    http.get<Task[]>(TASKS, { params }).then(r => r.data),
+
+  getById: (id: number) =>
+    http.get<Task>(`${TASKS}/${id}`).then(r => r.data),
+
+  create: (task: TaskCreate) =>
+    http.post<Task>(TASKS, task).then(r => r.data),
+
+  update: ({ id, ...data }: TaskUpdate) =>
+    http.patch<Task>(`${TASKS}/${id}`, data).then(r => r.data),
+
+  delete: (id: number) =>
+    http.delete(`${TASKS}/${id}`).then(r => r.data),
+};
